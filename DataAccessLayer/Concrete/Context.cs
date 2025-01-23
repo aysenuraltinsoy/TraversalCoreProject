b2486bde49c2f0263ea77e4c3cb0ad54cd742714
Administrator <Administrator@DESKTOP-6I89N2Q>
2025-01-23 21:27:02 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-6I89N2Q;database=TraversalDB;integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AboutSecond> AboutSeconds { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FeatureTwo> FeatureTwos { get; set; }
+        public DbSet<Guide> Guide { get; set; }
+        public DbSet<NewsLetter> NewsLetters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var x = new Context();
+            x.Remove(t);
+            x.SaveChanges();
+
+        }
+
+        public List<T> GetList()
+        {
+            using var x = new Context();
+            return x.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            using var x = new Context();
+            x.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            using var x = new Context();
+            x.Update(t);
+        }
+    }
+}
