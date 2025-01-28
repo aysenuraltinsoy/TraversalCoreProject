bc4c48679bb5b12d5d88d4c0b43ce1f4bd597abe
aysenur.altınsoy <a.altinsoyaysenur@gmail.com>
2025-01-28 22:41:43 +0300

    Add Partial Components and Managers

---
+++ TraversalCoreProject/ViewComponents/Default/_Feauture.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feauture : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            //ViewBag.image1 = featureManager.TGetById
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Statistics : ViewComponent
+    {
+      
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Guide.Count();
+            ViewBag.v3 = 382;
+            return View();
+        }
+    }
+}
