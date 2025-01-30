a139dd173926acf0e849fc28eb0c9564b4515e0a
aysenur.altınsoy <a.altinsoyaysenur@gmail.com>
2025-01-30 21:43:35 +0300

    Thema Change

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
@@ -12,5 +13,15 @@ namespace TraversalCoreProject.Controllers
             var values = destinationManager.TGetList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id) 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+        } 
     }
 }
